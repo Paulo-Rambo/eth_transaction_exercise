b7d06df60bb1d539eb5f222dabe0368712e1d2bb
Paulo Vitor <patoassado@gmail.com>
2023-08-03 14:10:47 -0300

    feat: metamask with goerli testnet transaction

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 const { default: Web3 } = require("web3");
 const { Chain, Common, Hardfork } = require("@ethereumjs/common");
 const { FeeMarketEIP1559Transaction } = require("@ethereumjs/tx");
+const dotenv = require("dotenv");
+dotenv.config();
 
-let web3 = new Web3("HTTP://127.0.0.1:7545");
+(async () => {
+  let web3 = new Web3(
+    "https://goerli.infura.io/v3/f6f0f3e54cc34206ad9dbcd62bf49891"
+  );
 
-let sendingAddress = "0xfC800c0BED134ceD288F9461cf13c375c6f2aDF2";
-let receivingAddress = "0x6b17C82167b35d55Fb3be542c76AF4A8e33AEbf6";
+  let sendingAddress = "0x21B267c4a4A9FFf308f06974B5095aeE4B21f4Ef";
+  let receivingAddress = "0x136ece6c16473185cdB9560eFb07ae938be6dA48";
 
-web3.eth.getBalance(sendingAddress).then(console.log);
-web3.eth.getBalance(receivingAddress).then(console.log);
+  await web3.eth.getBalance(sendingAddress).then(console.log);
+  await web3.eth.getBalance(receivingAddress).then(console.log);
 
-let newNonce = 0;
-web3.eth
-  .getTransactionCount(sendingAddress)
-  .then((transactionNum) => (newNonce = transactionNum));
+  let newNonce = 0;
+  await web3.eth
+    .getTransactionCount(sendingAddress)
+    .then((transactionNum) => (newNonce = transactionNum));
 
-let rawTransaction = {
-  nonce: newNonce,
-  to: receivingAddress,
-  maxPriorityFeePerGas: 8750000039,
-  maxFeePerGas: 8750000900,
-  gasPrice: 20000000,
-  gasLimit: 30000,
-  value: 12,
-  data: "",
-  chainId: 1337,
-};
+  let rawTransaction = {
+    nonce: newNonce,
+    to: receivingAddress,
+    maxPriorityFeePerGas: 8750000039,
+    maxFeePerGas: 8750000900,
+    gasPrice: 20000000,
+    gasLimit: 30000,
+    value: 1402831131312886n,
+    data: "",
+    chainId: 5,
+  };
 
-let privateKeySender =
-  "171b9111e395b1b82bc0cf66b47d19b2c2c8c21d41721d6379b93044f3fa4ee9";
+  let privateKeySender = process.env.PK_USER;
 
-const chain = new Common({
-  chain: Chain.Mainnet,
-  hardfork: Hardfork.Istanbul,
-});
-const tx = FeeMarketEIP1559Transaction.fromTxData(rawTransaction, { chain });
+  const chain = new Common({
+    chain: Chain.Mainnet,
+    hardfork: Hardfork.Istanbul,
+  });
+  const tx = FeeMarketEIP1559Transaction.fromTxData(rawTransaction, { chain });
 
-let privateKey = Buffer.from(privateKeySender, "hex");
-const signedTx = tx.sign(privateKey);
+  let privateKey = Buffer.from(privateKeySender, "hex");
+  const signedTx = tx.sign(privateKey);
 
-//test
-let serializedTx = signedTx.serialize();
-let rawTxHex = "0x" + serializedTx.toString("hex");
-console.log(rawTxHex);
+  //test
+  let serializedTx = signedTx.serialize();
+  let rawTxHex = "0x" + serializedTx.toString("hex");
+  console.log(rawTxHex);
 
-//send
-const serializedTransaction = signedTx.serialize();
-web3.eth.sendSignedTransaction(serializedTransaction);
+  //send
+  const serializedTransaction = signedTx.serialize();
+  await web3.eth.sendSignedTransaction(serializedTransaction);
 
-//verify
-web3.eth.getBalance(sendingAddress).then(console.log);
-web3.eth.getBalance(receivingAddress).then(console.log);
+  //verify
+  await web3.eth.getBalance(sendingAddress).then(console.log);
+  await web3.eth.getBalance(receivingAddress).then(console.log);
+})();
